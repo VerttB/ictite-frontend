300632a6d8be76c7ac605d0d5b454df2eecfe6d3
VerttB <alyssonoliveira456@gmail.com>
2025-06-09 11:39:41 -0300

    Ajustes da sidebar, tamanho, cores, formatado do layout

--- src/app/(paginas)/(public)/layout.tsx
+++ src/app/(paginas)/(public)/layout.tsx
@@ -7,16 +7,21 @@ export default function layout({
   children: React.ReactNode;
 }>){
     return(
-  <>
+  
        <SidebarProvider>
         <AppSidebar/>
+        <div className="flex flex-col bg-cinza-light pb-4 pr-4">
+        <Header></Header>
 
-          <main className="w-full">
-          <Header></Header>
+          <main className="w-full min-h-full bg-branco p-2  " style={{
+                      boxShadow: "inset 0px 0px 5px rgba(0, 0, 0, .5)",
+                      borderRadius: "10px 10px 0 0"
+                    }}>
             <SidebarTrigger/>
             {children}
           </main>
+          </div>
        </SidebarProvider>
-       </>
+      
     )
 }
--- src/app/(paginas)/(public)/page.tsx
+++ src/app/(paginas)/(public)/page.tsx
@@ -1,7 +1,17 @@
+'use client'
+import MapaRender from "@/components/Map";
+import dynamic from "next/dynamic";
+const Map = dynamic(() => import("@/components/Map"), {
+    loading: () => <p>Carregando..</p>,
+    ssr: false
+  })
 export default function page(){
     return(
-        <div className="w-full p-8 flex min-h-screen">
+        <div className="w-full p-8 flex flex-col space-betweeb  rounded-xl">
             <h1 className="h-fit text-xl rounded-xl bg-cinza-light p-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores earum quis in quos vel sequi exercitationem assumenda, quasi distinctio. Autem nostrum laboriosam harum aliquid assumenda nesciunt voluptatibus velit eveniet? Quisquam?</h1>
+            <div>
+                <Map/>
+            </div>
         </div>
     )
 }
