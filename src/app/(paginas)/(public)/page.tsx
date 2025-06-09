9ea671b71afbafc780efddf6e539d4ef2fc60692
Gabrielogui <gabrielcsro@gmail.com>
2025-06-09 16:55:53 -0300

    descomentando mapa

--- src/app/(paginas)/(public)/page.tsx
+++ src/app/(paginas)/(public)/page.tsx
@@ -12,7 +12,7 @@ export default function page(){
         <div className="w-full p-8 flex flex-col space-between rounded-xl">
             <h1 className="h-fit text-xl rounded-xl bg-cinza-light p-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores earum quis in quos vel sequi exercitationem assumenda, quasi distinctio. Autem nostrum laboriosam harum aliquid assumenda nesciunt voluptatibus velit eveniet? Quisquam?</h1>
             <div className='h-full  w-full  p-4'>
-                {/*<Map/>*/}
+                <Map/>
             </div>
 
             <Escola />
