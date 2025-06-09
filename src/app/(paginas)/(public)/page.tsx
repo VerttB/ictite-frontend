624ba3b3c50d8f981a1dd545c293c7aec965b9ae
Gabrielogui <gabrielcsro@gmail.com>
2025-06-09 08:52:49 -0300

    adicionando Escola na principal page

--- src/app/(paginas)/(public)/page.tsx
+++ src/app/(paginas)/(public)/page.tsx
@@ -1,7 +1,11 @@
+import Escola from "@/components/escola/Escola";
+
 export default function page(){
     return(
         <div className="w-full p-8 flex min-h-screen">
             <h1 className="h-fit text-xl rounded-xl bg-cinza-light p-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores earum quis in quos vel sequi exercitationem assumenda, quasi distinctio. Autem nostrum laboriosam harum aliquid assumenda nesciunt voluptatibus velit eveniet? Quisquam?</h1>
+
+            <Escola />
         </div>
     )
 }
---
+++ src/components/escola/Escola.tsx
@@ -0,0 +1,7 @@
+export default function Escola () {
+    return(
+        <div>
+            
+        </div>
+    );
+}
