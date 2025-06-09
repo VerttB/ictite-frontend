a8895eb404b6dc11ac5cd920eeb1eac69a6b5384
VerttB <alyssonoliveira456@gmail.com>
2025-06-09 12:18:35 -0300

    Finalizado o inicio do mapa

--- src/app/(paginas)/(public)/page.tsx
+++ src/app/(paginas)/(public)/page.tsx
@@ -5,13 +5,15 @@ const Map = dynamic(() => import("@/components/Mapa"), {
     loading: () => <p>Carregando..</p>,
     ssr: false
   })
+
+  
 export default function page(){
     return(
-        <div className="w-full p-8 flex flex-col space-betweeb  rounded-xl">
+        <div className="w-full p-8 flex flex-col space-between rounded-xl">
             <h1 className="h-fit text-xl rounded-xl bg-cinza-light p-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores earum quis in quos vel sequi exercitationem assumenda, quasi distinctio. Autem nostrum laboriosam harum aliquid assumenda nesciunt voluptatibus velit eveniet? Quisquam?</h1>
-            <div>
-                <Map/>
-            </div>
+            <div className='h-full  w-full  p-4'>
+            <Map/>
+        </div>
         </div>
     )
 }
