08f87d6f237723ea59fd7554a8a83e5be6a0381c
Gabrielogui <gabrielcsro@gmail.com>
2025-06-09 17:58:59 -0300

    Dados iniciais da escola no Drawer

--- src/components/escola/Escola.tsx
+++ src/components/escola/Escola.tsx
@@ -1,14 +1,38 @@
 import { MapPin } from "lucide-react";
-import { Drawer } from "../ui/drawer";
+import { Drawer, DrawerContent, DrawerTrigger } from "../ui/drawer";
 
 export default function Escola () {
     return(
         <div>
             <Drawer>
-                <div className="flex flex-row gap-3 items-center h-fit w-fit hover:scale-[105%] hover:cursor-pointer transition-all duration-200 p-2 rounded">
-                    <MapPin/>
-                    <p className="text-xl">Salvador-BA</p>
-                </div>
+                <DrawerTrigger>
+                    <div className="flex flex-row gap-3 items-center h-fit w-fit hover:scale-[105%] hover:cursor-pointer transition-all duration-200 p-2 rounded">
+                        <MapPin/>
+                        <p className="text-xl">Salvador-BA</p>
+                    </div>
+                </DrawerTrigger>
+                <DrawerContent>
+                    <div>
+                        <div className="flex flex-col gap-4 px-36 items-center">
+                            {/* IMAGEM */}
+                            <div className="bg-gray-300 h-60 w-60">
+
+                            </div>
+                            {/* TÍTULO E SUBTÍTULO */}
+                            <div className="flex flex-col gap-2 items-center">
+                                <p className="text-3xl font-bold">NOME DA ESCOLA</p>
+                                <p className="text-2xl font-semibold text-gray-400">CIDADE DA ESCOLA</p>
+                            </div>
+                            {/* DESCRIÇÃO */}
+                            <div className="text-2xl font-semibold text-gray-400">
+                                <p >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores earum quis in quos vel sequi exercitationem assumenda, quasi distinctio. Autem nostrum laboriosam harum aliquid assumenda nesciunt voluptatibus velit eveniet? Quisquam?</p>
+                            </div>
+                        </div>
+                        <div>
+                            
+                        </div>
+                    </div>
+                </DrawerContent>
             </Drawer>
         </div>
     );
