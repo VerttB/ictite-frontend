b3cd2b737bb9afb60d0639754d44d790341560e9
VerttB <alyssonoliveira456@gmail.com>
2025-06-09 08:35:14 -0300

    Mudanças na pagina home

--- src/app/(paginas)/(public)/page.tsx
+++ src/app/(paginas)/(public)/page.tsx
@@ -1,7 +1,7 @@
 export default function page(){
     return(
         <div className="w-full p-8 flex min-h-screen">
-            <h1 className="h-fit text-2xl rounded-xl bg-cinza-light p-8">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores earum quis in quos vel sequi exercitationem assumenda, quasi distinctio. Autem nostrum laboriosam harum aliquid assumenda nesciunt voluptatibus velit eveniet? Quisquam?</h1>
+            <h1 className="h-fit text-xl rounded-xl bg-cinza-light p-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores earum quis in quos vel sequi exercitationem assumenda, quasi distinctio. Autem nostrum laboriosam harum aliquid assumenda nesciunt voluptatibus velit eveniet? Quisquam?</h1>
         </div>
     )
 }
