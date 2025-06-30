fac47948a759e0c0414af0298c14f92d07efc045
Gabrielogui <gabrielcsro@gmail.com>
2025-06-30 17:36:29 -0300

    feat: Adicionando botão para fechar o Drawer do pesquisador

--- src/components/escola/Projeto.tsx
+++ src/components/escola/Projeto.tsx
@@ -1,8 +1,10 @@
+import { X } from "lucide-react";
+import { Button } from "../ui/button";
 import { Drawer, DrawerContent, DrawerDescription, DrawerHeader, DrawerTitle } from "../ui/drawer";
 
 interface ProjetoProps {
     isOpen: boolean;
-    onClose: () => void;
+    onClose: (open:boolean) => void;
 }
 
 export default function Projeto ({ isOpen, onClose } : ProjetoProps) {
@@ -10,6 +12,9 @@ export default function Projeto ({ isOpen, onClose } : ProjetoProps) {
         <Drawer open={isOpen} onOpenChange={onClose} direction="right" >
             <DrawerContent className="w-full">
                 <DrawerHeader>
+                    <div className="flex justify-start border-b items-center pb-2.5">
+                        <Button variant={"outline"} size={"icon"} onClick={() => onClose(false)}><X/></Button>
+                    </div>
                     <DrawerTitle>Nome do Projeto</DrawerTitle>
                     <DrawerDescription>Descrição do Projeto: Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam dolores, officia quidem hic culpa enim quae cumque autem provident temporibus labore quibusdam, tempore odit omnis ducimus inventore voluptate obcaecati modi!</DrawerDescription>
                 </DrawerHeader>
