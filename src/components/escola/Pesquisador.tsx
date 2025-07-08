7d2c67b955f142071fe24e5336847a83d2fe226d
Gabrielogui <gabrielcsro@gmail.com>
2025-07-08 09:06:02 -0300

    style: Cabeçalho do Drawer do Pesquisador

--- src/components/escola/Pesquisador.tsx
+++ src/components/escola/Pesquisador.tsx
@@ -1,4 +1,6 @@
-import { Drawer, DrawerContent, DrawerHeader, DrawerTitle } from "../ui/drawer";
+import Image from "next/image";
+import { Drawer, DrawerContent, DrawerDescription, DrawerHeader, DrawerTitle } from "../ui/drawer";
+import { MapPin } from "lucide-react";
 
 interface PesquisadorProps {
     isOpen: boolean;
@@ -9,9 +11,23 @@ export default function Pesquisador ({ isOpen, onClose } : PesquisadorProps) {
     return(
         <Drawer open={isOpen} onOpenChange={onClose} >
             <DrawerContent>
-                <DrawerHeader>
-                    <DrawerTitle>Pesquisador</DrawerTitle>
+                <DrawerHeader className="items-center">
+                    <div>
+                        <Image width={100} height={100} src={"https://picsum.photos/100/100"} alt="escola"
+                            className="rounded-md border border-cinza">
+                        </Image>
+                    </div>
+                    <DrawerTitle>Nome do Pesquisador</DrawerTitle>
+                    <div className="flex flex-row gap-1 items-center">
+                        <MapPin size={15} />
+                        <DrawerDescription className="font-semibold">Cidade da Escola</DrawerDescription>
+                    </div>
                 </DrawerHeader>
+
+                {/* CORPO DO DRAWER */}
+                <div>
+
+                </div>
             </DrawerContent>
         </Drawer>
 
