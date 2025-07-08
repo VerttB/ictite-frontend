29c31945eec976b7a90d606bed2b7f4406601374
verttb <alyssonoliveira456@gmail.com>
2025-07-08 20:21:47 -0300

    refactor: retornando null caso url null e check de isCancelled

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -4,29 +4,34 @@ export function useFetch<T = unknown>(url: string) {
   const [data, setData] = useState<T | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<Error | null>(null);
+    if(!url) return null;
 
 
-  console.log(`url do fetch ${url}`)
+
  useEffect(() => {
-
+  let isCancelled = false;
   const fetchedData = async () => {
+
     try{
     const res = await fetch(url);
     if(!res.ok) throw new Error("Erro ao buscar po dados");
     const jsonData = await res.json();
-
-    setData(jsonData);
+    if(!isCancelled) setData(jsonData);
     
     // eslint-disable-next-line
     }catch(err: any){
-      setError(err.message || "erro desconhecido")
+      setError(err.message || "erro desconhecido");
     }finally{
-      setLoading(false)
+      setLoading(false);
     }
   }
   
-  fetchedData()
+  fetchedData();
 
+  return () => {
+    isCancelled = true;
+  };
+  
  },[url])
 
  return {data, loading ,error}
