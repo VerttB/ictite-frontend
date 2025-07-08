906d8b4371faae887bba996c4c4adbf36a6cd74d
Gabrielogui <gabrielcsro@gmail.com>
2025-07-07 21:22:03 -0300

    style: Fazendo ajustes no Drawer de escola
    > Ajeitando padding
    > Diminuindo o tamanho do Header(imagem da escola ; Nome ; Cidade)
    > Diminuindo descrição
    > Ajeitando divs desnecessárias

--- src/app/(paginas)/(public)/page.tsx
+++ src/app/(paginas)/(public)/page.tsx
@@ -1,11 +1,11 @@
 import MapaComDrawer from "@/components/MapaComDrawer";
-import { GETictite } from "@/core/service/ICTITEService";
+//import { GETictite } from "@/core/service/ICTITEService";
 
 export default async function ProjetoPage() {
-  const projeto: IIctite = await GETictite();
+ // const projeto: IIctite = await GETictite();
   return (
     <div className="w-full p-8 flex flex-col space-between rounded-xl">
-      <h1 className="h-16 text-xl rounded-xl bg-cinza-light p-4">{projeto.objective}</h1>
+      <h1 className="h-16 text-xl rounded-xl bg-cinza-light p-4">{/*projeto.objective*/} Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus accusamus impedit fugit voluptate? Sint porro odio beatae consectetur sequi, iste assumenda pariatur quis asperiores non. Perspiciatis debitis fugit animi suscipit?</h1>
 
       <div className="h-full w-full p-4">
         <MapaComDrawer />
--- src/components/escola/Escola.tsx
+++ src/components/escola/Escola.tsx
@@ -1,5 +1,5 @@
-import { House, PanelsTopLeft, Printer } from "lucide-react";
-import { Drawer, DrawerContent, DrawerTitle } from "../ui/drawer";
+import { House, MapPin, PanelsTopLeft, Printer } from "lucide-react";
+import { Drawer, DrawerContent, DrawerDescription, DrawerHeader, DrawerTitle } from "../ui/drawer";
 import { Button } from "../ui/button";
 import CardPesquisador from "../card/CardPesquisador";
 import Image from "next/image";
@@ -25,68 +25,75 @@ export default function Escola ({open, onOpenChange}:EscolaProps) {
             <Drawer open={open} onOpenChange={onOpenChange}>
                 
                 <DrawerContent className="h-[95vh] flex flex-col">
-                    <div className="flex-1 overflow-y-auto px-4"> {/* FLEX-1 PARA PODER OCUPAR TODO O ESPAÇO DISPONÍVEL DO COMPONENTE */}
-                        <div className="flex flex-col gap-10 px-10 mb-5">
-                            <div className="flex flex-col gap-4 px-36 items-center">
-                                {/* IMAGEM */}
-                                <div className="">
-                                    <Image width={200} height={200} src={"https://picsum.photos/200/200"} alt="escola"
-                                        className="rounded-md border-4 border-cinza">
-                                    </Image>
-                                </div>
-                                {/* TÍTULO E SUBTÍTULO */}
-                                <div className="flex flex-col gap-2 items-center">
-                                    <DrawerTitle className="text-3xl font-bold">NOME DA ESCOLA</DrawerTitle>
-                                    <p className="text-2xl font-semibold text-gray-400">CIDADE DA ESCOLA</p>
-                                </div>
-                                {/* DESCRIÇÃO */}
-                                <div className="text-2xl font-semibold text-gray-400">
-                                    <p >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores earum quis in quos vel sequi exercitationem assumenda, quasi distinctio. Autem nostrum laboriosam harum aliquid assumenda nesciunt voluptatibus velit eveniet? Quisquam?</p>
-                                </div>
-                            </div>
-                            <div>
-                                <Tabs value={activeTab} onValueChange={setActiveTab}>
-                                    <TabsList className="flex flex-row gap-5 w-full py-2 px-4 rounded-md bg-blue-100">
-                                        <TabsTrigger value="pesquisadores" asChild>
-                                            <Button variant={activeTab === "pesquisadores" ? "default" : "outline"} 
-                                                className={`gap-2 ${activeTab === "pesquisadores" ? "bg-verde text-branco  hover:bg-branco hover:text-black" : "hover:bg-verde hover:text-branco"}`}> 
-                                                <House/> <p>Pesquisadores</p> 
-                                            </Button>
-                                        </TabsTrigger>
-                                        <TabsTrigger value="equipamentos" asChild>
-                                            <Button variant={activeTab === "equipamentos" ? "default" : "outline"} 
-                                                className={`gap-2 ${activeTab === "equipamentos" ? "bg-verde text-branco  hover:bg-branco hover:text-black" : "hover:bg-verde hover:text-branco"}`}> 
-                                                <Printer/> <p>Equipamentos</p>  
-                                            </Button>
-                                        </TabsTrigger>
-                                        <TabsTrigger value="projetos" asChild>
-                                            <Button variant={activeTab === "projetos" ? "default" : "outline"} 
-                                                className={`gap-2 ${activeTab === "projetos" ? "bg-verde text-branco hover:bg-branco hover:text-black" : "hover:bg-verde hover:text-branco"}`}>
-                                                <PanelsTopLeft/> <p>Projetos</p>
-                                            </Button>
-                                        </TabsTrigger>
-                                    </TabsList>
-                                    <TabsContent value="pesquisadores" className="mt-4">
-                                        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-5 justify-items-center">
-                                            <CardPesquisador onClick={() => setIsPesquisadorDrawerOpen(true)}/>
-                                            <CardPesquisador onClick={() => setIsPesquisadorDrawerOpen(true)}/>
-                                            <CardPesquisador onClick={() => setIsPesquisadorDrawerOpen(true)}/>
-                                            <CardPesquisador onClick={() => setIsPesquisadorDrawerOpen(true)}/>
-                                            <CardPesquisador onClick={() => setIsPesquisadorDrawerOpen(true)}/>
-                                            <CardPesquisador onClick={() => setIsPesquisadorDrawerOpen(true)}/>
-                                            <CardPesquisador onClick={() => setIsPesquisadorDrawerOpen(true)}/>
+                        
+                            <DrawerHeader className="">
+                                <div className="flex flex-col gap-4 items-center">
+                                    {/* IMAGEM */}
+                                    <div className="">
+                                        <Image width={100} height={100} src={"https://picsum.photos/100/100"} alt="escola"
+                                            className="rounded-md border border-cinza">
+                                        </Image>
+                                    </div>
+                                    {/* TÍTULO E SUBTÍTULO */}
+                                    <div className="flex flex-col gap-2 items-center">
+                                        <DrawerTitle className="text-2xl font-bold">NOME DA ESCOLA</DrawerTitle>
+                                        <div className="flex flex-row gap-1 items-center">
+                                            <MapPin size={15} />
+                                            <DrawerDescription className="font-semibold">CIDADE DA ESCOLA</DrawerDescription>
                                         </div>
-                                    </TabsContent>
-                                    <TabsContent value="equipamentos" className="mt-4">
-                                        <p>Equipamentos</p>
-                                    </TabsContent>
-                                    <TabsContent value="projetos" className="mt-4">
-                                        <p onClick={() => setIsProjetoDrawerOpen(true)}>Projetos</p>
-                                    </TabsContent>
-                                </Tabs>
+                                    </div>
+                                </div>
+                            </DrawerHeader>
+
+                            {/* CORPO DO DRAWER */}
+                            <div className="flex-1 flex flex-col min-h-0 overflow-hidden">
+                                <div className="py-4 px-12 overflow-y-auto">
+                                    {/* DESCRIÇÃO */}
+                                    <div className="text-sm mb-2 font-semibold text-gray-400">
+                                        <p >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores earum quis in quos vel sequi exercitationem assumenda, quasi distinctio. Autem nostrum laboriosam harum aliquid assumenda nesciunt voluptatibus velit eveniet? Quisquam?</p>
+                                    </div>
+                                    {/* MENU DE BOTÕES - TABS */}
+                                    <Tabs value={activeTab} onValueChange={setActiveTab}>
+                                        <TabsList className="flex flex-row gap-5 w-full py-2 px-4 rounded-md bg-blue-100">
+                                            <TabsTrigger value="pesquisadores" asChild>
+                                                <Button variant={activeTab === "pesquisadores" ? "default" : "outline"}
+                                                    className={`gap-2 ${activeTab === "pesquisadores" ? "bg-verde text-branco  hover:bg-branco hover:text-black" : "hover:bg-verde hover:text-branco"}`}>
+                                                    <House/> <p>Pesquisadores</p>
+                                                </Button>
+                                            </TabsTrigger>
+                                            <TabsTrigger value="equipamentos" asChild>
+                                                <Button variant={activeTab === "equipamentos" ? "default" : "outline"}
+                                                    className={`gap-2 ${activeTab === "equipamentos" ? "bg-verde text-branco  hover:bg-branco hover:text-black" : "hover:bg-verde hover:text-branco"}`}>
+                                                    <Printer/> <p>Equipamentos</p>
+                                                </Button>
+                                            </TabsTrigger>
+                                            <TabsTrigger value="projetos" asChild>
+                                                <Button variant={activeTab === "projetos" ? "default" : "outline"}
+                                                    className={`gap-2 ${activeTab === "projetos" ? "bg-verde text-branco hover:bg-branco hover:text-black" : "hover:bg-verde hover:text-branco"}`}>
+                                                    <PanelsTopLeft/> <p>Projetos</p>
+                                                </Button>
+                                            </TabsTrigger>
+                                        </TabsList>
+                                        <TabsContent value="pesquisadores" className="mt-4">
+                                            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-5 justify-items-center">
+                                                <CardPesquisador onClick={() => setIsPesquisadorDrawerOpen(true)}/>
+                                                <CardPesquisador onClick={() => setIsPesquisadorDrawerOpen(true)}/>
+                                                <CardPesquisador onClick={() => setIsPesquisadorDrawerOpen(true)}/>
+                                                <CardPesquisador onClick={() => setIsPesquisadorDrawerOpen(true)}/>
+                                                <CardPesquisador onClick={() => setIsPesquisadorDrawerOpen(true)}/>
+                                                <CardPesquisador onClick={() => setIsPesquisadorDrawerOpen(true)}/>
+                                                <CardPesquisador onClick={() => setIsPesquisadorDrawerOpen(true)}/>
+                                            </div>
+                                        </TabsContent>
+                                        <TabsContent value="equipamentos" className="mt-4">
+                                            <p>Equipamentos</p>
+                                        </TabsContent>
+                                        <TabsContent value="projetos" className="mt-4">
+                                            <p onClick={() => setIsProjetoDrawerOpen(true)}>Projetos</p>
+                                        </TabsContent>
+                                    </Tabs>
+                                </div>
                             </div>
-                        </div>
-                    </div>
                 </DrawerContent>
 
                 {/* |=======| DRAWER DO PESQUISADOR |=======| */}
