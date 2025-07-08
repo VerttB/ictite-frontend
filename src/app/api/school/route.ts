752d97212510461ece330ba1cb225f4eebaf10a8
verttb <alyssonoliveira456@gmail.com>
2025-07-08 19:43:55 -0300

    refactor: Fazendo mapa trabalhar com o banco

--- src/app/api/school/route.ts
+++ src/app/api/school/route.ts
@@ -4,9 +4,28 @@ import { NextResponse } from "next/server";
 export async function GET() {
     try{
         const result = await pool.query(`
-            SELECT name, city, ictite_id from school
+            SELECT name, city, ictite_id, latitude ,longitude from school
             `)
-        return  NextResponse.json(result.rows)
+
+            const features = result.rows.map((row: any) => ({
+                type: "Feature",
+                geometry: {
+                    type: "Point",
+                    coordinates: [row.longitude, row.latitude], 
+                },
+                properties: {
+                    id: row.id,
+                    name: row.name,
+                    city: row.city,
+                    ictite_id: row.ictite_id,
+                },
+                }));
+
+            const geoJson = {
+                collection:"FeatureCollection",
+                features
+            }
+        return  NextResponse.json(geoJson)
     }catch(e:any){
         console.log(e);
         return NextResponse.json({msg: "Erro ao dar fetch"})
