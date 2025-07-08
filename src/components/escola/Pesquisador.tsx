5d16d924a2c7d3a0564723271461cec8815a063f
Gabrielogui <gabrielcsro@gmail.com>
2025-07-08 16:30:10 -0300

    reafector: Drawer do pesquisador à direita
    > style: DrawerHeader atualizado para Drawer lateral à direita

--- src/components/escola/Pesquisador.tsx
+++ src/components/escola/Pesquisador.tsx
@@ -1,34 +1,43 @@
 import Image from "next/image";
 import { Drawer, DrawerContent, DrawerDescription, DrawerHeader, DrawerTitle } from "../ui/drawer";
-import { MapPin } from "lucide-react";
+import { MapPin, X } from "lucide-react";
+import { Button } from "../ui/button";
 
 interface PesquisadorProps {
     isOpen: boolean;
-    onClose: () => void;
+    onClose: (open:boolean) => void;
 }
 
 export default function Pesquisador ({ isOpen, onClose } : PesquisadorProps) {
     return(
-        <Drawer open={isOpen} onOpenChange={onClose} >
+        <Drawer open={isOpen} onOpenChange={onClose} direction="right" >
             <DrawerContent>
-                <DrawerHeader className="items-center">
-                    <div>
-                        <Image width={100} height={100} src={"https://picsum.photos/100/100"} alt="escola"
-                            className="rounded-md border border-cinza">
-                        </Image>
+                <DrawerHeader className="shadow">
+                    <div className="flex justify-start border-b items-center pb-2.5">
+                        <Button variant={"outline"} size={"icon"} onClick={() => onClose(false)}><X/></Button>
                     </div>
-                    <DrawerTitle>Nome do Pesquisador</DrawerTitle>
-                    <div className="flex flex-row gap-1 items-center">
-                        <MapPin size={15} />
-                        <DrawerDescription className="font-semibold">Cidade da Escola</DrawerDescription>
+                    <div className="flex flex-row gap-2 items-center justify-center">
+                        <div>
+                            <Image width={100} height={100} src={"https://picsum.photos/100/100"} alt="escola"
+                                className="rounded-md border border-cinza">
+                            </Image>
+                        </div>
+                        <div className="items-center">
+                            <DrawerTitle>Nome do Pesquisador</DrawerTitle>
+                            <div className="flex flex-row gap-1 items-center">
+                                <MapPin size={15} />
+                                <DrawerDescription className="font-semibold">Cidade da Escola</DrawerDescription>
+                            </div>
+                        </div>
                     </div>
                 </DrawerHeader>
 
                 {/* CORPO DO DRAWER */}
-                <div className="flex-1 flex flex-col min-h-0 overflow-hidden">
+                <div className="flex-1 flex flex-col p-2 overflow-hidden">
                     <div className="text-sm text-justify text-gray-500 ">
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur odio corporis temporibus dignissimos laborum sequi voluptates non, vitae reiciendis laudantium! Soluta provident ratione placeat. Molestiae sequi aspernatur aliquid ipsam eligendi.</p>
                     </div>
+                    
                 </div>
             </DrawerContent>
         </Drawer>
