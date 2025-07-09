85a9be96d1d7a8d81126c6be40f433b5dd43255c
verttb <alyssonoliveira456@gmail.com>
2025-07-09 11:56:16 -0300

    useFetch mudanças

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -4,11 +4,12 @@ export function useFetch<T = unknown>(url: string) {
   const [data, setData] = useState<T | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<Error | null>(null);
-    if(!url) return null;
 
 
 
  useEffect(() => {
+
+  if(!url) return;
   let isCancelled = false;
   const fetchedData = async () => {
 
@@ -31,7 +32,7 @@ export function useFetch<T = unknown>(url: string) {
   return () => {
     isCancelled = true;
   };
-  
+
  },[url])
 
  return {data, loading ,error}
