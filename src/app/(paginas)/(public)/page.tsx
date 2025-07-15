27a862258d9405cc19b8e7437f7b9b959aafd850
Gabrielogui <gabrielcsro@gmail.com>
2025-07-15 20:20:17 -0300

    style: Adicionando detalhes ao texto do menu principal

--- src/app/(paginas)/(public)/page.tsx
+++ src/app/(paginas)/(public)/page.tsx
@@ -5,7 +5,16 @@ export default async function ProjetoPage() {
  // const projeto: IIctite = await GETictite();
   return (
     <div className="w-full p-8 flex flex-col space-between rounded-xl">
-      <h1 className="h-16 text-xl text-justify rounded-xl bg-cinza-light p-4">{/*projeto.objective*/} Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus accusamus impedit fugit voluptate? Sint porro odio beatae consectetur sequi, iste assumenda pariatur quis asperiores non. Perspiciatis debitis fugit animi suscipit?</h1>
+      <div className="flex flex-row w-full gap-4">
+        <div className="h-32 sm:text-sm md:text-3xl rounded-xl bg-vermelho p-4 w-64 shadow-xl
+                        flex justify-center items-center font-bold
+        ">
+          <h1>ICTITE</h1>
+        </div>
+        <div className="h-32 sm:text-sm md:text-xl text-justify rounded-xl w-full bg-cinza-light p-4 overflow-y-auto shadow-xl">
+          <h1 className="h-16">{/*projeto.objective*/} Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus accusamus impedit fugit voluptate? Sint porro odio beatae consectetur sequi, iste assumenda pariatur quis asperiores non. Perspiciatis debitis fugit animi suscipit?</h1>
+        </div>
+      </div>
 
       <div className="h-full w-full p-4">
         <MapaComDrawer />
