6a21c5b479c3d359bb9c1dd9a3030fd93baafcdb
Gabrielogui <gabrielcsro@gmail.com>
2025-07-21 11:38:14 -0300

    style: Fazendo div superior com Título e botão para voltar à página inicial

--- src/app/(paginas)/(public)/revistas/page.tsx
+++ src/app/(paginas)/(public)/revistas/page.tsx
@@ -1,7 +1,19 @@
+import { Button } from "@/components/ui/button";
+import { ChevronLeft } from "lucide-react";
+import Link from "next/link";
+
 export default function Revistas () {
+    
     return(
         <div className="w-full p-8">
-            Revistas
+            <div className="flex flex-row gap-4 items-center">
+                <Link href={"/"}>
+                    <Button size={"icon"} variant={"outline"}>
+                        <ChevronLeft size={20} />
+                    </Button>
+                </Link>
+                <h1 className="text-2xl font-semibold">Revistas</h1>
+            </div>
         </div>
     );
 }
