c805f89b52e7db642c13e597e7351a45ed876a34
verttb <alyssonoliveira456@gmail.com>
2025-07-21 16:01:14 -0300

    feat: pegando equipamentos projetos e pesquisadores em escola

--- src/app/api/school/[id]/route.ts
+++ src/app/api/school/[id]/route.ts
@@ -5,11 +5,59 @@ export async function GET(
   { params }: { params: Promise<{ id: string }> }
 ){
     const { id } = await params
-    console.log(id)
+ 
     try{
-         const result = await pool.query(
-      `SELECT name,city,id,photo_id,description FROM school 
-       WHERE id = $1`,
+          const result = await pool.query(
+      `
+            SELECT 
+                s.id,
+                s.name,
+                s.city,
+                s.photo_id,
+                s.description,
+
+                COALESCE(
+                (
+                    SELECT json_agg(json_build_object(
+                    'name', r.name,
+                    'sex', r.sex,
+                    'race', r.race,
+                    'type', r.type
+                    ))
+                    FROM researcher r
+                    WHERE r.school_id = s.id
+                ), '[]'
+                ) AS researchers,
+
+                COALESCE(
+                (
+                    SELECT json_agg(json_build_object(
+                    'id', p.id,
+                    'name', p.name,
+                    'description', p.description
+                    ))
+                    FROM project p
+                    WHERE p.school_id = s.id
+                ), '[]'
+                ) AS projects,
+
+                COALESCE(
+                (
+                    SELECT json_agg(json_build_object(
+                    'id', e.id,
+                    'name', te.name,          -- Pegando o nome do type_equipment
+                    'description', te.description, -- Pegando a descrição do type_equipment
+                    'photo', te.photo         -- Pegando a foto do type_equipment
+                    ))
+                    FROM equipment e
+                    JOIN type_equipment te ON e.type_equipment_id = te.id -- JOIN para acessar a tabela type_equipment
+                    WHERE e.school_id = s.id
+                ), '[]'
+                ) AS equipment
+
+            FROM school s
+            WHERE s.id = $1
+            `,
       [id]
     );
             
@@ -21,6 +69,6 @@ export async function GET(
         return  NextResponse.json(result.rows[0])
     }catch(e:any){
         console.log(e);
-        return NextResponse.json({msg: "Erro ao dar fetch em escolas/id"})
+        return NextResponse.json(e)
     }
 }
