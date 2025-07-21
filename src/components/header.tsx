130f94f453bd4e1233c578724ea0ca791b178642
verttb <alyssonoliveira456@gmail.com>
2025-07-21 14:11:08 -0300

    feat: imagens de apoio e footer

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,8 +2,8 @@ import Image from "next/image"
 import {Button} from "../components/ui/button"
 export const Header = () => {
     return(
-        <div className="w-full p-3 bg-cinza-light flex justify-between items-center">
-            <div>
+        <header className="w-full p-3 bg-cinza-light flex justify-between items-center">
+            <div className="flex justify-between gap-4">
                 <Image
                 src={"/logoImagem.png"}
                 alt="Logo"
@@ -13,12 +13,40 @@ export const Header = () => {
                     inline-block 
                 "
             />
+            <Image
+                        src={"/apoios/UnebLogo.jpg"}
+                        alt="Logo"
+                        width={128}
+                        height={64}
+                        className="inline-block object-contain h-16"
+                                />
+                        <Image
+                        src={"/apoios/fapesbLogo.jpg"}
+                        alt="Logo"
+                        width={128}
+                        height={64}
+                        className="inline-block object-contain  h-16"
+                        />           
+                        <Image
+                        src={"/apoios/governoDoEstadologo.png"}
+                        alt="Logo"
+                        width={128}
+                        height={64}
+                        className="inline-block object-contain  h-16"
+                        />   
+                         <Image
+                        src={"/apoios/fiocruzLogo.png"}
+                        alt="Logo"
+                        width={128}
+                        height={64}
+                        className="inline-block object-contain  h-16"
+                        /> 
 
         
             </div>
             <Button  className="text-xl cursor-pointerrounded-xl py-1 h-fit px-10">
                 Fazer Login
             </Button>
-        </div>
+        </header>
     )
 }
