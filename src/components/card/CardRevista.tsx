1b936ecd33e10c4d3d3b719353c4bbc8cb40c33d
Gabrielogui <gabrielcsro@gmail.com>
2025-07-26 10:06:02 -0300

    chore: Colocando exemplos mock nas revistas

--- src/components/card/CardRevista.tsx
+++ src/components/card/CardRevista.tsx
@@ -1,12 +1,17 @@
 import { Copy, SquareArrowOutUpRight } from "lucide-react";
 import { Button } from "../ui/button";
+import { Revista } from "@/core/interface/Revista";
 
-export default function CardRevista () {
+interface CardRevistaProps {
+    revista: Revista;
+}
+
+export default function CardRevista ({ revista }: CardRevistaProps) {
     return(
         <div className="flex flex-col justify-between p-4 border rounded-md w-[250px] h-[200px] bg-gray-50">
             <div>
-                <h2 className="text-xl font-semibold">Título da Revista</h2>
-                <p className="text-gray-500 line-clamp-3">Descrição da revista: Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non sint magni quas nesciunt et, dignissimos doloribus repellat blanditiis modi, quae debitis neque nulla fuga optio eligendi ipsam amet numquam quisquam.</p>
+                <h2 className="text-xl font-semibold">{revista.title}</h2>
+                <p className="text-gray-500 line-clamp-3">{revista.description}</p>
             </div>
             <div className="flex flex-row gap-2 justify-end w-full">
                 <Button size={"icon"} variant={"ghost"} className="cursor-pointer">
