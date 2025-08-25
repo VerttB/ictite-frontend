4118e143ea7d364d772e7f8955a818ef9a5f59c9
Gabrielogui <gabrielcsro@gmail.com>
2025-08-25 10:29:41 -0300

    style: Superior dos materiais didáticos

--- src/app/(paginas)/(public)/materiais/page.tsx
+++ src/app/(paginas)/(public)/materiais/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { ChevronLeft } from "lucide-react";
+
 export default function Materiais () {
     return(
-        <div>
-            Materiais Didáticos
+        <div className="w-full px-8 py-4">
+            <div className="flex flex-row gap-5 items-center">
+                <Button size={"icon"} variant={"outline"} className="cursor-pointer"><ChevronLeft /></Button>
+                <p className="text-2xl font-semibold">Materiais Didáticos</p>
+            </div>
         </div>
     );
 }
