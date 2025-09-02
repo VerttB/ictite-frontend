c14c7ec79f362fd3ec1d40eeceb063b6895349ca
Gabrielogui <gabrielcsro@gmail.com>
2025-09-02 19:50:40 -0300

    style: Fazendo card das categorias dos materiais

---
+++ src/components/card/CardMateriais.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function CardMateriais() {
+    return(
+        <div className="h-[280px] w-[220px] bg-vermelho rounded-sm shadow-sm">
+            <div className="flex flex-col gap-3 w-full items-center justify-center pt-2.5">
+                <Image src={"https://picsum.photos/100/100"} alt="Material" width={100} height={100}
+                    className="rounded-full"
+                ></Image>
+                <h2 className="text-2xl text-white font-semibold">Título</h2>
+                <p className="px-1.5 text-justify line-clamp-4 text-white">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est ullam perferendis enim iure commodi nemo eveniet, voluptatibus perspiciatis quas inventore consectetur velit. Doloremque odio blanditiis aliquid rem corrupti. Optio, numquam.</p>
+            </div>
+        </div>
+    );
+}
