633feb55e3e049ca25204fff1097c855ab49f2d0
Gabrielogui <gabrielcsro@gmail.com>
2025-09-08 09:02:00 -0300

    style: Colocando busca na página inicial

--- src/app/(paginas)/(public)/page.tsx
+++ src/app/(paginas)/(public)/page.tsx
@@ -1,4 +1,7 @@
 import MapaComDrawer from "@/components/MapaComDrawer";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Search } from "lucide-react";
 //import { GETictite } from "@/core/service/ICTITEService";
 
 export default async function ProjetoPage() {
@@ -15,6 +18,15 @@ export default async function ProjetoPage() {
         >
           <h1 className="">{/*projeto.objective || */"O objetivo da Rede Interdisciplinar de Ciência, Tecnologia e Inovação em Territórios Escolares (Rede-ICTITE) é promover a popularização da ciência e fortalecer a educação científica e para a saúde nas redes estadual e municipais de ensino da Bahia, por meio da construção colaborativa de uma rede integrada de professores, estudantes e instituições, estimulando o protagonismo jovem e ampliando o alcance das ações educativas e de divulgação científica nos territórios escolares."}</h1>
         </div>
+      </div>
+
+      <div className="relative flex-grow w-full mt-5">
+        <Input type={"search"} placeholder="Busca por escola, pesquisador ou projetos" 
+            className="pl-4 pr-10 py-2 w-full rounded-lg border-2"/>
+        <Button variant={"ghost"} size={"icon"}
+            className="absolute right-1 top-1/2 transform -translate-y-1/2 h-8 w-8 cursor-pointer">
+            <Search/>
+        </Button>
       </div>
 
       <div className="flex flex-col gap-1 h-full w-full p-2 mt-5 
