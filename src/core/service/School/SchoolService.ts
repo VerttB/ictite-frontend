128868c0129672252ba94cf3e06668eb98cc3811
Verttb <alyssonoliveirai456@gmail.com>
2025-09-09 13:32:11 -0300

    feat: Implement school data fetching
    - Implement fetching of school data, including by ID, and retrieve associated researchers, projects, and equipment.

--- src/core/service/School/SchoolService.ts
+++ src/core/service/School/SchoolService.ts
@@ -1,3 +1,7 @@
+import { Equipment } from "@/core/interface/Equipment";
+import { Researcher } from "@/core/interface/Pesquisador/Researcher";
+import { Project } from "@/core/interface/Project";
+
 export const getSchoolGeoData =  async () => {
     try{
         const res  = await fetch("http://localhost:8000/schools/geojson")
@@ -6,7 +10,7 @@ export const getSchoolGeoData =  async () => {
             throw new Error(`Erro na busca: ${res.status} ${res.statusText}`);
         }
 
-        const  data = res.json()
+        const  data = await res.json()
 
         return data
     } catch (e) {
@@ -15,9 +19,60 @@ export const getSchoolGeoData =  async () => {
     }
 }
 
-export const getSchoolFull =  async (id: string) => {
+export const getSchoolById =  async (id: string, full?:boolean) => {
     try{
-        const res  = await fetch("http://localhost:8000/schools/geojson")
+        const res  = await fetch(`http://localhost:8000/schools/${id}?full=${full}`)
+        
+        if (!res.ok) {
+            throw new Error(`Erro na busca: ${res.status} ${res.statusText}`);
+        }
+
+        const  data = await res.json()
+
+        return data
+    } catch (e) {
+        console.error("Falha ao buscar dados de escolas:", e);
+        return null; 
+    }
+}
+
+export const getSchools = async () => {
+     try{
+        const res  = await fetch(`http://localhost:8000/schools`)
+        
+        if (!res.ok) {
+            throw new Error(`Erro na busca: ${res.status} ${res.statusText}`);
+        }
+
+        const  data = await res.json()
+
+        return data
+    } catch (e) {
+        console.error("Falha ao buscar dados de escolas:", e);
+        return null; 
+    }
+}
+
+export const getSchoolResearchers = async (id:string): Promise<Researcher[]> => {
+     try{
+        const res  = await fetch(`http://localhost:8000/schools/${id}/researchers`)
+        
+        if (!res.ok) {
+            throw new Error(`Erro na busca: ${res.status} ${res.statusText}`);
+        }
+
+        const data:Researcher[] = await res.json()
+
+        return data
+    } catch (e) {
+        console.error("Falha ao buscar dados de escolas:", e);
+        return []; 
+    }
+}
+
+export const getSchoolProjects = async (id:string):Promise<Project[]> => {
+     try{
+        const res  = await fetch(`http://localhost:8000/schools/${id}/projects`)
         
         if (!res.ok) {
             throw new Error(`Erro na busca: ${res.status} ${res.statusText}`);
@@ -27,9 +82,24 @@ export const getSchoolFull =  async (id: string) => {
 
         return data
     } catch (e) {
-        console.error("Falha ao buscar dados de escolas:", e);
-        return null; 
+        console.error("Falha ao buscar projetos de escolas:", e);
+        return []; 
     }
 }
 
+export const getSchoolEquiments = async (id:string):Promise<Equipment[]> => {
+     try{
+        const res  = await fetch(`http://localhost:8000/schools/${id}/equipments`)
+        
+        if (!res.ok) {
+            throw new Error(`Erro na busca: ${res.status} ${res.statusText}`);
+        }
 
+        const  data = res.json()
+
+        return data
+    } catch (e) {
+        console.error("Falha ao buscar equipamentos de escolas:", e);
+        return []; 
+    }
+}
