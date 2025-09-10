10ff3f8d781ebb03843230cd10482926e08881fe
Verttb <alyssonoliveirai456@gmail.com>
2025-09-10 09:19:43 -0300

    feat: Adiciona loading e  layout da página escola
    - Adiciona um componente de loading para a página de escola.
    
    - Ajusta o layout da página de detalhes da escola, incluindo espaçamento, tamanho de fontes e dimensões da imagem.

--- src/app/(paginas)/(public)/escola/[id]/page.tsx
+++ src/app/(paginas)/(public)/escola/[id]/page.tsx
@@ -7,29 +7,30 @@ import { EscolaTabs } from "@/components/escola/EscolaTabs"
 export default async function Page({
   params,
 }: {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 
 }){
     
-    const { id } = params
+    const { id } = await params
     const school = await getSchoolById(id)
     if(!school) throw new Error("Erro")
     return(
              
-                    <div className="flex-1  px-4"> 
-                        <div className="flex flex-col gap-4 px-10 mb-5">
+                    <div className="flex flex-col gap-8  px-10"> 
+                        
+                        <div className="flex flex-col gap-12 mb-5">
                                 {/* IMAGEM */}
                                 <div className="flex gap-4 items-center">
-                                <div className="flex w-4/5 min-h-64 justify-center relative">
+                                <div className="flex max-w-92 w-4/5 min-h-64 justify-center rounded-lg relative">
                                     <Image fill src={"https://images.unsplash.com/photo-1591123120675-6f7f1aae0e5b?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} alt="escola"
-                                        className="rounded-sm border-3 shadow-lg border-white">
+                                        className="rounded-lg p-2 border-3 shadow-lg border-white">
                                     </Image>
                                 </div>
                                 
                                 <div className="flex flex-col gap-2 text-center">
                                     <div className="text-4xl">{school.name}</div>
-                                    <p className="text-3xl font-semibold text-gray-400">{school.city}</p>
-                                    <p className="text-3xl font-semibold text-gray-400" >{school.description}</p>
+                                    <p className="text-2xl font-semibold text-gray-400">{school.city}</p>
+                                    <p className="text-2xl font-semibold text-gray-400" >{school.description}</p>
                                 </div>
                             </div>
                             </div>
