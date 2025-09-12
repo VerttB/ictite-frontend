3e7446fac0e447b5c4cf0b8dab7ca1ad8c470c9e
Gabrielogui <gabrielcsro@gmail.com>
2025-09-12 10:37:06 -0300

    chore: Modificação na rota e url base do env

--- src/core/service/ICTITEService.ts
+++ src/core/service/ICTITEService.ts
@@ -1,9 +1,9 @@
 import { Ictite } from "../interface/IIctite";
 
-const url_base = process.env.URL_BASE
+const url_base = process.env.NEXT_PUBLIC_BASE_URL
 export const  GETictite = async ():Promise<Ictite> => {
     try{
-        const res: Response = await fetch(`${url_base}/project`);
+        const res: Response = await fetch(`${url_base}/ictite`);
         if(!res) throw new Error(`Erro: ${res}`)
         const data = await res.json();
         // if (Array.isArray(data) && data.length > 0) {
