132de853aedfab67517a97fd966cd03bc3d7b71a
Verttb <alyssonoliveirai456@gmail.com>
2025-09-14 18:53:03 -0300

    feat: overload getResaercherById Agora Pode retornar diferentes tipos baseado no parametro full

--- src/core/service/Pesquisador/PesquisadorService.ts
+++ src/core/service/Pesquisador/PesquisadorService.ts
@@ -1,3 +1,5 @@
+import { Researcher } from "@/core/interface/Pesquisador/Researcher";
+import { ResearcherFinal } from "@/core/interface/Pesquisador/ResearcherFinal";
 
 export const  getResearchers = async (name: string) => {
     try{
@@ -8,6 +10,20 @@ export const  getResearchers = async (name: string) => {
 
     }catch(e: any){
         console.error(e);
+    }
+}
+export function getResearcherById(researcherId: string, full: false): Promise<Researcher | null>;
+export function getResearcherById(researcherId: string, full: true): Promise<ResearcherFinal | null>;
+export async function getResearcherById (researcherId:string ,full: boolean = false) {
+    try{
+        const res: Response = await fetch(`http://localhost:8000/researchers/${researcherId}?full=${full}`);
+        if(!res) throw new Error(`Erro: ${res}`)
+        const data = await res.json();
+        return data;
+
+    }catch(e: any){
+        console.error(e);
+        return null
     }
 }
 
