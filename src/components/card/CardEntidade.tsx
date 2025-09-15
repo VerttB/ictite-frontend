ac0561a7d98a93531909952ae92e76f6373b1137
Gabrielogui <gabrielcsro@gmail.com>
2025-09-15 15:41:39 -0300

    refactor: Colocanod para receber a entidade como um todo ao inves do nome e icon

--- src/components/card/CardEntidade.tsx
+++ src/components/card/CardEntidade.tsx
@@ -1,16 +1,21 @@
 import { LucideIcon } from "lucide-react";
 
-interface CardEntidadeProps {
-  icon: LucideIcon;
-  nome: string;
+interface EntidadeProps {
+    nome  : string;
+    icon  : LucideIcon;
+    campos: string[]
 }
 
-export default function CardEntidade ({ icon: Icon, nome}: CardEntidadeProps) { 
+interface CardEntidadeProps {
+  entidade: EntidadeProps
+}
+
+export default function CardEntidade ({ entidade }: CardEntidadeProps) { 
     return(
         <div className="flex flex-col items-center justify-center h-28 w-28 bg-cinza-light rounded-sm
                         hover:shadow-sm hover:shadow-verde hover:border-2 hover:border-verde transition-all cursor-pointer">
-            <Icon size={50}/>
-            <span className="font-semibold">{nome}</span>
+            <entidade.icon size={50}/>
+            <span className="font-semibold">{entidade.nome}</span>
         </div>
     );
 }
