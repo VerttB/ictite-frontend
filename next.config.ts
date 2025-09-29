55c2c1f41861dc5fa150914a0221d762a0d3b616
Verttb <alyssonoliveirai456@gmail.com>
2025-09-29 17:30:21 -0300

    refactor: deixando imagens não otimizadas

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["picsum.photos", "200.128.66.226", "images.unsplash.com", "plus.unsplash.com"]
+    domains: ["picsum.photos", "200.128.66.226", "images.unsplash.com", "plus.unsplash.com"],
+    unoptimized: true
     
   },
   output:'standalone'
