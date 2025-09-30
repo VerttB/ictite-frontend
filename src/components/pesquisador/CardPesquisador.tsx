bccddb7ab0bead070e7f2fc1d89a55c95b468534
Gabrielogui <gabrielcsro@gmail.com>
2025-09-30 18:29:24 -0300

    fix: Botão de fechar Drawer funcionando
    > Colocando componente do Pesquisador fora da Div do card

--- src/components/pesquisador/CardPesquisador.tsx
+++ src/components/pesquisador/CardPesquisador.tsx
@@ -15,34 +15,34 @@ export default function CardPesquisador ({ onClick, researcher }: CardPesquisado
     const [openDrawer, setOpenDrawer] = useState(false);
     
     return(
-        <div  onClick={onClick ?  onClick : () => setOpenDrawer(true)} 
-            className="relative border-2 h-[250px] w-[200px] rounded-md p-2 
-                    cursor-pointer overflow-hidden bg-black group
-            ">
+        <>
+            <div  onClick={onClick ?  onClick : () => setOpenDrawer(true)}
+                className="relative border-2 h-[250px] w-[200px] rounded-md p-2
+                        cursor-pointer overflow-hidden bg-black group
+                ">
             
-            <div className={`absolute inset-0 
-                     bg-cover bg-center 
-                    opacity-70 group-hover:opacity-45 transition-opacity duration-300
-`}>
-            <Image fill alt="Imagem do pesquisador" className="rounded-t-lg object-cover" src={researcher.image ? `${researcher.image}` : "https://picsum.photos/100/100"}/>
-          </div>
-
-            <div className="relative p-2 flex flex-col h-full justify-end gap-2">
-                <div className="text-white">
-                    <p className=" text-xl ">{researcher.name}</p>
-                    <div className="hidden group-hover:block ">
-                        <p>{researcher.sex}</p>
-                        <p>{researcher.race}</p>
+                <div className={`absolute inset-0
+                         bg-cover bg-center
+                        opacity-70 group-hover:opacity-45 transition-opacity duration-300
+            `}>
+                <Image fill alt="Imagem do pesquisador" className="rounded-t-lg object-cover" src={researcher.image ? `${researcher.image}` : "https://picsum.photos/100/100"}/>
+              </div>
+                <div className="relative p-2 flex flex-col h-full justify-end gap-2">
+                    <div className="text-white">
+                        <p className=" text-xl ">{researcher.name}</p>
+                        <div className="hidden group-hover:block ">
+                            <p>{researcher.sex}</p>
+                            <p>{researcher.race}</p>
+                        </div>
+                    </div>
+                    <div className="flex items-center justify-center gap-2
+                                bg-vermelho text-white py-1 px-3 rounded-sm w-fit text-md ">
+                        <Book  size={20}/>
+                        <p className="mb-1">{researcher.type}</p>
                     </div>
                 </div>
-                <div className="flex items-center justify-center gap-2
-                            bg-vermelho text-white py-1 px-3 rounded-sm w-fit text-md ">
-                    <Book  size={20}/>
-                    <p className="mb-1">{researcher.type}</p>
-                </div>
             </div>
-            {openDrawer && <Pesquisador isOpen={openDrawer} researcherId={researcher.id}  onClose={() => {setOpenDrawer(false)}}/>}
-
-        </div>
+            <Pesquisador isOpen={openDrawer} researcherId={researcher.id}  onClose={() => {setOpenDrawer(false)}}/>
+        </>
     );
 }
