a98b15a876155f53367f716ceed4ed92dc0b80d3
Verttb <alyssonoliveirai456@gmail.com>
2025-10-04 12:29:14 -0300

    style: Adapta design para mobile e espaçamento de botões
    - Adapta o cabeçalho para dispositivos móveis, aprimora o design e adiciona um hook para detecção de tamanho de tela.
    - Ajusta o tamanho padrão do botão para telas menores e adiciona um espaçamento horizontal em telas maiores.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,44 +6,45 @@ import Link from "next/link"
 import { getAssetPrefix } from "@/core/utils/api";
 import { useTheme } from "@/core/providers/ThemeProvider";
 import { MoonIcon, SunIcon } from "lucide-react"
+import { useIsMobile } from "@/hooks/use-mobile"
 const isProd = process.env.NODE_ENV === "production";
 
 export const Header = () => {
     const { theme, toggleTheme } = useTheme()
+    const isMobile = useIsMobile();
     return(
         <header className="w-full p-3 bg-foreground flex justify-between items-center">
-            <div className="flex justify-between gap-4">
+            <div className="flex flex-col sm:flex-row   w-full h-full sm:gap-2">
+            <div className="relative w-32  sm:w-48 h-16">
                 <Image
                 src={`${getAssetPrefix()}/logoImagem.png`}
                 alt="Logo"
-                width={192}
-                height={192}
+                fill
                 className="
-                    inline-block 
-                "
-            />
+                    object-contain"/>
+            </div>
                 <p className="text-sm text-gray-500 flex items-end">beta β 1.1</p>
 
         
             </div>
-            <div className="flex flex-row gap-4">
+            <div className="flex flex-row items-center gap-2 sm:gap-4">
                 <Button 
                     size={"icon"}
                     variant="outline"
                     onClick={toggleTheme}
-                    className="text-xl cursor-pointer rounded-md"
+                    className="text-xl cursor-pointer "
                     title={theme === 'light' ? 'Mudar para tema escuro' : 'Mudar para tema claro'}>
                     {theme === 'light' ? <MoonIcon /> : <SunIcon />}
                 </Button>
                 <Link href={"/console"}>
-                    <Button variant={"outline"}  className="text-xl  cursor-pointerrounded-xl cursor-pointer py-1 px-10">
+                    <Button variant={"outline"}  className="text-xl cursor-pointer py-1 px-10">
                         <Bolt />
-                        Console
+                        {isMobile ? "" : "Console"}
                     </Button>
                 </Link>
-                <Button  className="text-xl cursor-pointerrounded-xl cursor-pointer py-1  px-10">
+                <Button  className="text-xl  py-1  px-10">
                     <CircleUserRound />
-                    Fazer Login
+                    {isMobile ? "Login" : "Fazer Login"}
                 </Button>
             </div>
         </header>
