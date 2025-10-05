a387ba8c737abc82b849d612842bfd20dd62d04a
Gabrielogui <gabrielcsro@gmail.com>
2025-10-05 10:13:41 -0300

    feat: Colocando uma tabela de exemplo em console para melhor UX no campos da entidade
    > Adicionando componente Table do shadcn

--- src/components/console/CamposEntidades.tsx
+++ src/components/console/CamposEntidades.tsx
@@ -1,5 +1,6 @@
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "../ui/dialog";
 import { LucideIcon } from "lucide-react";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "../ui/table";
 
 interface EntidadeProps {
     nome  : string;
@@ -27,7 +28,26 @@ export default function CamposEntidades ( { isOpen, onClose, entidade }:CamposEn
                             ))}
                         </div>
                     </div>
+
                 </DialogHeader>
+                <div>
+                    <Table>
+                        <TableHeader className="bg-transparent">
+                            <TableRow className=" *:border-border hover:bg-transparent [&>:not(:last-child)]:border-r border">
+                                {entidade.campos.map((campo) => (
+                                    <TableHead className="text-black text-center" key={campo}>{campo}</TableHead>
+                                ))}
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody className="[&_td:first-child]:rounded-l-lg [&_td:last-child]:rounded-r-lg border">
+                            <TableRow className="*:border-border hover:bg-transparent [&>:not(:last-child)]:border-r">
+                                {Array.from({ length: entidade.campos.length }).map((_, i) => (
+                                    <TableCell  key={i} className="text-center">exemplo01</TableCell>
+                                ))}
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </div>
             </DialogContent>
         </Dialog>
     );
