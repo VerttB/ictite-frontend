61f5b16f643ac5093341bb2a6b3a21c68b919ebd
Gabrielogui <gabrielcsro@gmail.com>
2025-10-07 11:09:48 -0300

    Style: Mudando um pouco o estílo da página de escola

--- src/app/(paginas)/(public)/escolas/[id]/page.tsx
+++ src/app/(paginas)/(public)/escolas/[id]/page.tsx
@@ -7,6 +7,7 @@ import Image from "next/image"
 import { EscolaTabs } from "@/components/escola/EscolaTabs"
 import { useParams } from "next/navigation";
 import InfoBar from "@/components/InfoBar";
+import { Book, LucideIcon, MapPin, Table, Table2 } from "lucide-react";
 
 export default function Page(){
   const { id } = useParams<{ id: string }>();
@@ -15,14 +16,14 @@ export default function Page(){
 
   const loading = (!school && !schoolError) || (!schoolStatistics && !statsError)
   const error = schoolError || statsError
-  let stats: { titulo: string; valor: number }[] = []
+  let stats: { titulo: string; valor: number; Icon: LucideIcon }[] = []
   if(schoolStatistics)
     stats =[{
-      titulo: "Pesquisadores", valor: schoolStatistics.researchers
+      titulo: "Pesquisadores", valor: schoolStatistics.researchers, Icon: Book
     },{
-      titulo: "Projetos", valor: schoolStatistics.projects
+      titulo: "Projetos", valor: schoolStatistics.projects, Icon: Table2
     },{
-      titulo: "Equipamentos", valor: schoolStatistics.equipments
+      titulo: "Equipamentos", valor: schoolStatistics.equipments, Icon: Table
     }]
   if (loading) return (
     <div className="px-10 py-6">Carregando...</div>
@@ -42,13 +43,18 @@ export default function Page(){
         {/* IMAGEM */}
           <div className="flex w-full max-w-92 sm:w-4/5 min-h-64 justify-center rounded-lg relative">
             <Image fill src={"https://images.unsplash.com/photo-1591123120675-6f7f1aae0e5b?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} alt="escola"
-              className="rounded-lg p-2 border-3 shadow-lg border-white" />
+              className="rounded-lg p-2 border-3 shadow-xs border-white/60" />
           </div>
 
           <div className="flex flex-col gap-2 text-justify sm:text-left sm:w-2/3">
             <div className="text-2xl">{school.name}</div>
-            <p className="text-lg font-semibold text-font-primary/60">{school.city}</p>
-            <p className="text-lg font-semibold text-font-primary/50">{school.description}</p>
+            <div className="flex flex-row gap-2 items-center">
+              <MapPin className="text-lg text-font-primary/60" />
+              <p className="text-lg font-semibold text-font-primary/60">{school.city}</p>
+            </div>
+            <div className="p-4 bg-foreground rounded-md border">
+              <p className="text-lg font-semibold text-font-primary/50">{school.description}</p>
+            </div>
           </div>
       </div>
 
--- src/components/InfoBar.tsx
+++ src/components/InfoBar.tsx
@@ -1,16 +1,20 @@
+import { LucideIcon } from "lucide-react"
+
 interface InfoBarProps{
     titulo : string,
-    valor : number
+    valor  : number,
+    Icon   : LucideIcon 
 }
 
 export default function InfoBar(props: { data: InfoBarProps[] }){
     return(
-        <div className="flex gap-8 p-4 sm:py-10 text-center bg-foreground rounded-md w-full justify-around border-1 border-border  ">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-4 bg-foreground rounded-md shadow-sm border  ">
 
-            {props.data.map(({titulo, valor}) => (
-               <div key={titulo} className="flex flex-col">
+            {props.data.map(({titulo, valor, Icon}) => (
+               <div key={titulo} className="flex flex-col items-center text-center p-3 rounded-md bg-primary transition-all duration-300 hover:scale-95 border text-white">
+                <Icon />
                 <span className="font-light text-lg sm:text-xl">{titulo}</span>
-                <span className="font-extrabold text-3xl sm:text-5xl">{valor}</span>
+                <span className="font-extrabold text-3xl sm:text-4xl">{valor}</span>
                </div>
             ))}
         </div>
