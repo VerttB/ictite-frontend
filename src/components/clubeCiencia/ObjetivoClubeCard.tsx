369082b96d0f3a0582a2164c87d5240a503a8391
Gabrielogui <gabrielcsro@gmail.com>
2025-11-11 10:32:33 -0300

    style: Adicionando hover estilizado para card de objetivo do clube de ciencia

--- src/components/clubeCiencia/ObjetivoClubeCard.tsx
+++ src/components/clubeCiencia/ObjetivoClubeCard.tsx
@@ -2,12 +2,13 @@ import { FlaskConical } from "lucide-react";
 
 export default function ObjetivoClubeCard () {
     return(
-        <div className="w-[300px] flex flex-col gap-3 bg-foreground p-4 rounded-md border">
+        <div className="w-[300px] flex flex-col gap-3 bg-foreground p-4 rounded-md border 
+                        hover:shadow-sm hover:shadow-primary hover:border-2 hover:border-primary transition-all">
             <div className="flex gap-2 items-center text-primary">
                 <FlaskConical />
                 <h3 className="text-2xl font-semibold">Objetivo 01</h3>
             </div>
-            <div className="line-clamp-4">
+            <div className="line-clamp-4 text-justify">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ipsa quis illo dignissimos laborum, asperiores neque nisi iste, a non commodi repudiandae tempore explicabo nihil dolore totam rerum quibusdam ratione!
             </div>
         </div>
