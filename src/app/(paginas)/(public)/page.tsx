5d1e769d1d9785b3a901cf2a678e1a406e07c3bb
Gabrielogui <gabrielcsro@gmail.com>
2025-11-18 10:20:32 -0300

    feat: Adicionando posição para notícias na página principal

--- src/app/(paginas)/(public)/page.tsx
+++ src/app/(paginas)/(public)/page.tsx
@@ -12,6 +12,7 @@ import { SugestionBase } from "@/core/interface/SugestionBase";
 import PesquisadorLoader from "@/components/pesquisador/PesquisadorLoader";
 import ProjetoLoader from "@/components/projeto/ProjetoLoader";
 import ClubeCienciaLoader from "@/components/clubeCiencia/ClubeCienciaLoader";
+import Noticias from "@/components/noticias/Noticias";
 
 export default function ProjetoPage() {
 
@@ -38,6 +39,13 @@ export default function ProjetoPage() {
 
   return (
     <div className="w-full px-2 py-2 sm:px-8 flex flex-col gap-4 space-between rounded-xl">
+
+      <div>
+        <Noticias />
+      </div>
+
+
+      {/* |=======| OBJETIVO DO ICTITE |=======| */}
       <div className="flex flex-row w-full gap-4">
         <div 
           className="
