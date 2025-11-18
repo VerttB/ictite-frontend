858d9ef43705be0c7c54d4f4922ec8c835b5030e
Verttb <alyssonoliveirai456@gmail.com>
2025-11-18 04:06:21 -0300

    feat: Pesquisadores em console adm
    Adiciona card de pesquisadores ao console do adm
    Cria função para criar novos pesquisadores

---
+++ src/components/console/PesquisadorAdm.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { Section } from "../Section";
+import useSWR from "swr";
+import { getSchools } from "@/core/service/SchoolService";
+import { useState } from "react";
+import { PersonStanding } from "lucide-react";
+import { createResearcher, getResearchers } from "@/core/service/PesquisadorService";
+import { PesquisadorType } from "@/schemas/PesquisadorSchema";
+import { PesquisadorAddModal } from "../pesquisador/PesquisadorAddModal";
+export const PesquisadorAdm = () => {
+
+  const { data: pesquisadores, isLoading, mutate } = useSWR(
+    "pesquisadores",
+    () => getResearchers("")
+  );
+
+  const { data: escolas } = useSWR("escolas", () => getSchools());
+  const [open, setOpen] = useState(false);
+  const onSubmit = async (data: PesquisadorType) => {
+   
+    await createResearcher(data);
+    mutate();      
+    setOpen(false); 
+  };
+
+  if (!pesquisadores) return null;
+  return (
+    <>
+      <Section
+        title="Pesquisadores"
+        items={pesquisadores}
+        icon={<PersonStanding />}
+        onAdd={() => setOpen(true)}
+      />
+
+      <PesquisadorAddModal
+        open={open}
+        onClose={() => setOpen(false)}
+        escolas={escolas || []}
+        onSubmit={onSubmit}
+      />
+    </>
+  );
+};
+    
--- src/core/service/PesquisadorService.ts
+++ src/core/service/PesquisadorService.ts
@@ -2,6 +2,7 @@ import { Researcher } from "@/core/interface/Pesquisador/Researcher";
 import { ResearcherFinal } from "@/core/interface/Pesquisador/ResearcherFinal";
 import { Project } from "@/core/interface/Project";
 import { getBaseUrl } from "@/core/utils/api";
+import { PesquisadorType } from "@/schemas/PesquisadorSchema";
 
 export const  getResearchers = async (name: string) => {
     try{
@@ -49,6 +50,25 @@ export async function getResearchersByClube (clubeId: string): Promise<Researche
         const data = await res.json();
         return data;
 
+    }catch(e: unknown){
+        console.error(e);
+        return null
+    }
+}
+
+export async function createResearcher(newResearcher: PesquisadorType) {
+    console.log("Creating researcher...", newResearcher);
+    try{
+        const res: Response = await fetch(`${getBaseUrl()}/researchers/`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newResearcher)
+        });
+        if(!res) throw new Error(`Erro: ${res}`)
+        const data = await res.json();
+        return data;
     }catch(e: unknown){
         console.error(e);
         return null
