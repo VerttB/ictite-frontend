e29d905e914b35f649f7e3f5febed844e2eae762
Verttb <alyssonoliveirai456@gmail.com>
2025-11-19 23:33:07 -0300

    feat: Adicionando possibilida de mascara para inputfield

--- src/components/ui/FormInputField.tsx
+++ src/components/ui/FormInputField.tsx
@@ -4,15 +4,17 @@ import { Input } from "./input";
 interface InputFieldProps {
   name: string;
   label: string;
+  mask?: (value: string) => string;
 }
 
-export const InputField = ({ name, label }: InputFieldProps) => {
-  const { register, formState: { errors } } = useFormContext();
+export const InputField = ({ name, label, mask }: InputFieldProps) => {
+  const { register, formState: { errors }, getValues } = useFormContext();
 
   return (
     <div>
       <label className="block text-sm mb-1">{label}</label>
-      <Input {...register(name)} />
+      <Input {...register(name)}
+              onChange={(e) => e.target.value = mask ? mask(e.target.value) : e.target.value}  />
       {errors[name] && <p className="text-red-500 text-sm">{errors[name].message as string}</p>}
     </div>
   );
