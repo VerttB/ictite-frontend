8f7e17f864bf756ea059e05fac51b361abdc8bac
Verttb <alyssonoliveirai456@gmail.com>
2025-12-12 13:53:49 -0300

    refactor: Ajusta interfaces para refletir retorno da api

--- src/core/interface/Clube/ClubeCiencia.ts
+++ src/core/interface/Clube/ClubeCiencia.ts
@@ -1,12 +1,11 @@
 import { Image } from "../Image";
+import { School } from "../School";
 
 export interface ClubeCiencia {
     id: string;
     name: string;
     description: string;
-    school: {
-        id: string;
-        name: string;}
+    school: Pick<School, "id" | "name">;
     images: Image[];
 }
 
