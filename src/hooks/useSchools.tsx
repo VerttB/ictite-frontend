51c18a29d5524036926b6ab1c394d89ad015a9e8
Gabrielogui <gabrielcsro@gmail.com>
2025-12-18 11:19:28 -0300

    chore: descomentando hooks de retorno dos pesquisadores e projetos + tipando com researcher

--- src/hooks/useSchools.tsx
+++ src/hooks/useSchools.tsx
@@ -1,32 +1,33 @@
+import { Researcher } from "@/core/interface/Pesquisador/Researcher";
 import { Project } from "@/core/interface/Project";
-import { getSchoolEquiments } from "@/core/service/SchoolService";
+import { getSchoolEquiments, getSchoolProjects, getSchoolResearchers } from "@/core/service/SchoolService";
 import useSWR from "swr";
 
-// export const useSchoolResearchers = (id: string) => {
-//     const { data, isLoading, error } = useSWR(
-//         id ? `school-${id}-researchers` : null,
-//         () => getSchoolResearchers(id)
-//     );
+export const useSchoolResearchers = (id: string) => {
+    const { data, isLoading, error } = useSWR<Researcher[]>(
+        id ? `school-${id}-researchers` : null,
+        () => getSchoolResearchers(id)
+    );
 
-//     return {
-//         researchers: data,
-//         isLoading,
-//         errorReseachers: error,
-//     };
-// };
+    return {
+        researchers: data,
+        isLoading,
+        errorReseachers: error,
+    };
+};
 
-// export const useSchoolProjects = (id: string) => {
-//     const { data, isLoading, error } = useSWR<Project[]>(
-//         id ? `school-${id}-projects` : null,
-//         () => getSchoolProjects(id)
-//     );
+export const useSchoolProjects = (id: string) => {
+    const { data, isLoading, error } = useSWR<Project[]>(
+        id ? `school-${id}-projects` : null,
+        () => getSchoolProjects(id)
+    );
 
-//     return {
-//         projects: data,
-//         isLoading,
-//         errorProjects: error,
-//     };
-// };
+    return {
+        projects: data,
+        isLoading,
+        errorProjects: error,
+    };
+};
 
 export const useSchoolEquipments = (id: string) => {
     const { data, isLoading, error } = useSWR(
