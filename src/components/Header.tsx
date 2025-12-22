385b9ac24e23e61802b22d5c42abaab06492521f
Gabrielogui <gabrielcsro@gmail.com>
2025-12-22 11:05:37 -0300

    feat: Adicionando logo de cor branca quando o tema for dark

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,19 +13,25 @@ export const Header = () => {
     const { theme, toggleTheme } = useTheme();
     const { isAuthenticated } = useUserContext();
     const { isMobile } = useViewPort();
+
+
+    const logoSrc = theme === "dark"
+      ? `${getAssetPrefix()}/nova_logo_ictite_branca.png` // logo branca para tema escuro
+      : `${getAssetPrefix()}/nova_logo_ictite.png`; // logo normal para tema claro
+
     return (
         <header className="bg-foreground flex w-full items-center justify-between p-3">
             <div className="flex h-full w-full flex-col sm:flex-row sm:gap-2">
                 <div className="relative h-16 w-32 sm:w-48">
                     <Image
-                        src={`${getAssetPrefix()}/nova_logo_ictite.png`}
+                        src={logoSrc}
                         alt="Logo"
                         fill
                         className="object-contain"
                     />
                 </div>
                 <p className="flex items-end text-sm text-gray-500">
-                    beta β 1.1
+                    beta β 2.0
                 </p>
             </div>
             <div className="flex flex-row items-center gap-2 sm:gap-4">
