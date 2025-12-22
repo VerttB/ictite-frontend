bdc5da4b2d7fe412b38908ce88933f4a377e0ddc
Verttb <alyssonoliveirai456@gmail.com>
2025-12-22 03:13:04 -0300

    refactor: Alterando username por email

--- src/schemas/LoginSchema.ts
+++ src/schemas/LoginSchema.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
 export const userLoginSchema = z.object({
-    username: z.string().min(1, "O campo de nome não deve estar vazio"),
+    email: z.string().min(1, "O campo de email não deve estar vazio"),
     password: z.string().min(1, "O campo da senha não deve estar vazio"),
 });
