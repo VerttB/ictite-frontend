88ae1ddd94e1e5161afa992d95dc243bffa72578
Verttb <alyssonoliveirai456@gmail.com>
2025-12-23 13:38:09 -0300

    fix: Corrigido layout não aparecendo

---
+++ src/app/(paginas)/(adm)/layout.tsx
@@ -0,0 +1,31 @@
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/AppSidebar";
+import { Toaster } from "@/components/ui/sonner";
+import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
+export default function layout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <SidebarProvider>
+            <AppSidebar />
+            <div className="bg-foreground flex w-full flex-col pr-4 pb-4">
+                <Header></Header>
+
+                <main
+                    className="bg-background h-full w-full p-2"
+                    style={{
+                        boxShadow: "inset 0px 0px 5px rgba(0, 0, 0, .5)",
+                        borderRadius: "10px 10px 10px 10px",
+                    }}>
+                    <SidebarTrigger />
+                    <Toaster />
+                    {children}
+                </main>
+                <Footer />
+            </div>
+        </SidebarProvider>
+    );
+}
