dbefdceddf790d38fc874ef07d40c46940a20289
Verttb <alyssonoliveirai456@gmail.com>
2025-12-31 01:59:41 -0300

    WIP: Funcionalidade para trocar tipo de lista em section de flex para grid e vice versa

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -10,17 +10,21 @@ import {
     DropdownMenuLabel,
     DropdownMenuSeparator,
 } from "@radix-ui/react-dropdown-menu";
-
+import { useState } from "react";
+import { Grid, List } from "lucide-react";
+import { Button } from "./ui/button";
 type ItemType = {
     name: string;
     images?: Image[];
     image?: string;
+    extra?: any[];
 };
 
 type SectionProps = {
     title: string;
     items: ItemType[];
     icon?: React.ReactNode;
+    children?: React.ReactNode;
     onAdd?: () => void;
     onUpdate?: (item: any) => void;
     onDelete?: (item: any) => void;
@@ -38,18 +42,46 @@ const resolveImage = (item: ItemType) => {
     return undefined;
 };
 
-export const Section = ({ title, items, icon, onAdd }: SectionProps) => {
-    console.log(items);
+export const Section = ({
+    title,
+    items,
+    icon,
+    onAdd,
+    children,
+}: SectionProps) => {
+    const [layout, setLayout] = useState<"grid" | "list">("grid");
     return (
-        <section className="mb-10">
-            <div className="mb-4 flex h-8 items-center border-b-2 border-gray-300">
-                <div className="flex items-center gap-2">
+        <section className="flex-1 px-4">
+            <div className="mb-4 flex items-center justify-between border-b-2 border-gray-300">
+                <div className="flex items-center">
                     {icon}
                     <h2 className="text-lg font-semibold">{title}</h2>
+                    <p className="bg-primary ml-4 rounded-md px-1 py-0.5 text-sm text-white">
+                        Total: {items.length}
+                    </p>
                 </div>
-                <p className="bg-primary ml-4 rounded-md px-1 py-0.5 text-sm text-white">
-                    Total: {items.length}
-                </p>
+                <div className="ml-4 h-12 w-full">{children}</div>
+                <div className="mb-2 ml-auto flex items-center gap-2">
+                    <Button
+                        variant={"ghost"}
+                        onClick={() => setLayout("list")}
+                        className="px-0.5 py-0 hover:bg-gray-200"
+                        title="Layout em lista">
+                        <List
+                            size={20}
+                            className={
+                                layout === "list" ? "text-font-primary" : ""
+                            }
+                        />
+                    </Button>
+                    <Button
+                        variant={"ghost"}
+                        onClick={() => setLayout("grid")}
+                        className="px-0.5 py-0 hover:bg-gray-200"
+                        title="Layout em grade">
+                        <Grid size={20} color="black" className="" />
+                    </Button>
+                </div>
             </div>
             <div className="flex flex-wrap items-center gap-3">
                 {items.map((item, i) => (
