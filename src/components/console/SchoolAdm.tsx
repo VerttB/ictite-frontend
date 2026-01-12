057bee3a09363f6516d89c560d8d77db4a7a72a9
Verttb <alyssonoliveirai456@gmail.com>
2026-01-12 11:56:59 -0300

    feat: página de escola com editar e excluir

--- src/components/console/SchoolAdm.tsx
+++ src/components/console/SchoolAdm.tsx
@@ -4,7 +4,9 @@ import { Section } from "../Section";
 import useSWR from "swr";
 import {
     createSchool,
+    deleteSchool,
     getSchools,
+    updateSchool,
     uploadSchoolImage,
 } from "@/core/service/SchoolService";
 import { useState } from "react";
@@ -14,12 +16,19 @@ import { ControlledImageUpload } from "../ui/ControlledImageInput";
 import { InputField } from "../ui/FormInputField";
 import { mask } from "@/lib/maskBuilder";
 import { SchoolSearchParams } from "@/core/domain/School";
-import { SchoolCreate, SchoolCreateSchema } from "@/core/domain/School";
+import {
+    SchoolCreate,
+    SchoolCreateSchema,
+    SchoolUpdate,
+    SchoolUpdateSchema,
+    School,
+} from "@/core/domain/School";
 import { SearchAndFilter } from "../SearchAndFilter";
 import { ItemFilterConfig } from "@/core/interface/ItemFilterConfig";
 import { Pagination } from "../Pagination";
 
 import { useUrlPagination } from "@/hooks/useUrlPagination";
+import { DeleteConfirmationModal } from "../DeleteConfirmationModal";
 interface SchoolAdmProps {
     params: SchoolSearchParams;
 }
@@ -33,19 +42,50 @@ export const SchoolAdm = ({ params }: SchoolAdmProps) => {
         keepPreviousData: true,
     });
 
-    const [open, setOpen] = useState(false);
+    const [isCreating, setIsCreating] = useState(false);
+    const [editingItem, setEditingItem] = useState<School | null>(null);
+    const [deletingItem, setDeletingItem] = useState<School | null>(null);
 
     const { applyFilters, changePage } = useUrlPagination();
 
     const onSubmit = async (data: SchoolCreate) => {
-        const { id } = await createSchool(data);
-        const form = new FormData();
-        data.images.forEach((image) => {
-            form.append("images", image);
-        });
-        await uploadSchoolImage(id, form);
-        mutate();
-        setOpen(false);
+        try {
+            const { id } = await createSchool(data);
+            const form = new FormData();
+            data.images.forEach((image) => {
+                form.append("images", image);
+            });
+
+            await uploadSchoolImage(id, form);
+            mutate();
+            setIsCreating(false);
+        } catch (error) {
+            console.error("Error creating school:", error);
+        }
+    };
+
+    const onSubmitUpdate = async (data: SchoolUpdate) => {
+        if (!editingItem) return;
+        try {
+            await updateSchool(editingItem.id, data);
+            mutate();
+        } catch (error) {
+            console.error("Error updating school:", error);
+        } finally {
+            setEditingItem(null);
+        }
+    };
+
+    const onSubmitDelete = async () => {
+        if (!deletingItem) return;
+        try {
+            await deleteSchool(deletingItem.id);
+            mutate();
+        } catch (error) {
+            console.error("Error deleting school:", error);
+        } finally {
+            setDeletingItem(null);
+        }
     };
 
     const filters: ItemFilterConfig[] = [
@@ -56,11 +96,13 @@ export const SchoolAdm = ({ params }: SchoolAdmProps) => {
 
     return (
         <div className="flex h-full w-full flex-col">
-            <Section
+            <Section<School>
                 title="Escolas"
                 items={paginatedSchools.items}
                 icon={<School2 />}
-                onAdd={() => setOpen(true)}>
+                onAdd={() => setIsCreating(true)}
+                onUpdate={(school) => setEditingItem(school)}
+                onDelete={(school) => setDeletingItem(school)}>
                 <SearchAndFilter
                     currentParams={params as any}
                     applyParams={applyFilters}
@@ -71,8 +113,8 @@ export const SchoolAdm = ({ params }: SchoolAdmProps) => {
             </Section>
 
             <BaseFormModal<typeof SchoolCreateSchema, SchoolCreate>
-                open={open}
-                onClose={() => setOpen(false)}
+                open={isCreating}
+                onClose={() => setIsCreating(false)}
                 onSubmit={onSubmit}
                 title="Adicionar Escola"
                 schema={SchoolCreateSchema}
@@ -83,6 +125,33 @@ export const SchoolAdm = ({ params }: SchoolAdmProps) => {
 
                 <ControlledImageUpload name="images" />
             </BaseFormModal>
+
+            {editingItem && (
+                <BaseFormModal<typeof SchoolUpdateSchema, SchoolUpdate>
+                    open={!!editingItem}
+                    key={editingItem.id}
+                    onClose={() => setEditingItem(null)}
+                    onSubmit={onSubmitUpdate}
+                    title="Atualizar Escola"
+                    schema={SchoolUpdateSchema}
+                    props={{
+                        defaultValues: {
+                            name: editingItem?.name,
+                            description: editingItem?.description,
+                        },
+                    }}>
+                    <InputField name="name" label="Nome da Escola" />
+                    <InputField name="description" label="Descrição" />
+                </BaseFormModal>
+            )}
+
+            <DeleteConfirmationModal
+                open={!!deletingItem}
+                onClose={() => setDeletingItem(null)}
+                onConfirm={onSubmitDelete}
+                title={`Deseja excluir ${deletingItem?.name}?`}
+                description="Essa ação não pode ser desfeita."
+            />
             <Pagination
                 currentPage={paginatedSchools.page}
                 onLoadMore={changePage}
