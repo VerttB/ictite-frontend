ecf6145908cc3107c56c6cf96ef8169a90a2845b
Verttb <alyssonoliveirai456@gmail.com>
2026-01-13 18:13:25 -0300

    feat: Criando constante para validação do ano

--- src/core/domain/Project.ts
+++ src/core/domain/Project.ts
@@ -3,21 +3,32 @@ import { SimpleIdNameSchema, SearchParamParser } from "../../schemas/Common";
 import { ImageSchema, ImageCreateSchema } from "./Image";
 import { ResearcherSchema } from "./Researcher";
 import { PaginationSearchParamsSchema } from "@/schemas/Pagination";
+import { yearValidation } from "../constants/validation";
 
 export const ProjectSchema = z.object({
     id: z.uuid(),
     name: z.string().min(1, "Nome obrigatório"),
     description: z.string().min(1, "Descrição obrigatória"),
     description_long: z.string().min(1, "Descrição longa é obrigatória"),
-    year: z.number().int().nonnegative(),
+    year: z
+        .string()
+        .length(4, "Ano obrigatório")
+        .refine(
+            (val) => {
+                const yearNum = Number(val);
+                return yearNum >= yearValidation.min && yearNum <= yearValidation.max;
+            },
+            { message: "Ano inválido" }
+        ),
     clube: SimpleIdNameSchema,
     images: ImageSchema.array(),
 });
 
 export const ProjectCreateSchema = ProjectSchema.pick({
-    id: true,
+    name: true,
     description_long: true,
     description: true,
+    year: true,
 }).and(
     z.object({
         clube_ciencia_id: z.uuid(),
