d6949f981f887d8d92c75cf7ac9842a1b3ac64b7
Verttb <alyssonoliveirai456@gmail.com>
2026-01-19 15:33:53 -0300

    refactor: Tipando filtros

--- src/core/utils/searchParamBuilder.ts
+++ src/core/utils/searchParamBuilder.ts
@@ -1,14 +1,16 @@
+import { SearchParams } from "../interface/SearchParams";
+
 export const buildSearchParameters = (
-    filters: Record<string, any>,
+    filters: SearchParams,
     existingParams?: URLSearchParams
 ): string => {
     const params = new URLSearchParams(existingParams?.toString() || "");
     Object.entries(filters).forEach(([key, value]) => {
         if (value !== undefined && value !== null) {
             if (Array.isArray(value)) {
-                value.forEach((val) => params.append(key, val));
+                value.forEach((val) => params.append(key, val.toString()));
             } else {
-                params.append(key, value);
+                params.append(key, value.toString());
             }
         } else {
             params.delete(key);
