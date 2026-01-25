1072a6b9742ce7d3d29483dfa77876dbd502180b
Verttb <alyssonoliveirai456@gmail.com>
2026-01-24 23:26:52 -0300

    refactor: Tornando página dinamica

--- src/app/(paginas)/(public)/page.tsx
+++ src/app/(paginas)/(public)/page.tsx
@@ -1,24 +1,21 @@
+export const dynamic = "force-dynamic";
 
 import { GETictite } from "@/core/service/ICTITEService";
 import Mapa from "@/components/Mapa";
 import SearchClient from "@/components/SearchList";
 
-
 export default async function ProjetoPage() {
-    
-   const projeto = await GETictite();
+    const projeto = await GETictite();
 
     return (
         <div className="space-between flex w-full flex-col gap-4 rounded-xl px-2 py-2 sm:px-8">
             <div className="flex w-full flex-row gap-4">
                 <div className="bg-foreground border-border w-full rounded-md border-2 px-4 py-6 text-justify indent-4">
-                    <h1 className="sm:text-sm md:text-xl">
-                        {projeto.objective}
-                    </h1>
+                    <h1 className="sm:text-sm md:text-xl">{projeto.objective}</h1>
                 </div>
             </div>
 
-           <SearchClient/>
+            <SearchClient />
             <Mapa />
         </div>
     );
