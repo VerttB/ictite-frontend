4d0fe2437d90772013afaf770c38e1073732622e
Verttb <alyssonoliveirai456@gmail.com>
2026-02-13 16:59:44 -0300

    refactor: Utilizando useSWR para requisição

--- src/components/noticias/Noticias.tsx
+++ src/components/noticias/Noticias.tsx
@@ -1,53 +1,59 @@
-"use client"; // Se estiver usando Next.js App Router
+"use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import CardNoticias from "./CardNoticias";
 import CardSecundariaNoticias from "./CardSecundariaNoticias";
 import { InstagramPost } from "@/core/domain/Instagram";
 import { getInstagramPosts } from "@/core/service/InstagramService";
+import useSWR from "swr";
 
 export default function Noticias() {
-    const [posts, setPosts] = useState<InstagramPost[]>([]);
+    //const [posts, setPosts] = useState<InstagramPost[]>([]);
+    const {
+        data: posts,
+        isLoading,
+        error,
+    } = useSWR("instagramPosts", () => getInstagramPosts());
     const [selectedPost, setSelectedPost] = useState<InstagramPost | null>(null);
-    const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const fetchPosts = async () => {
-            try {
-                const data = await getInstagramPosts();
-                setPosts(data);
-                if (data.length > 0) {
-                    setSelectedPost(data[0]); 
-                }
-            } catch (error) {
-                console.error("Erro ao carregar notícias:", error);
-            } finally {
-                setLoading(false);
-            }
-        };
+    // useEffect(() => {
+    //     const fetchPosts = async () => {
+    //         try {
+    //             const data = await getInstagramPosts();
+    //             setPosts(data);
+    //             if (data.length > 0) {
+    //                 setSelectedPost(data[0]);
+    //             }
+    //         } catch (error) {
+    //             console.error("Erro ao carregar notícias:", error);
+    //         } finally {
+    //             setLoading(false);
+    //         }
+    //     };
 
-        fetchPosts();
-    }, []);
+    //     fetchPosts();
+    // }, []);
 
-    if (loading) return <div>Carregando notícias...</div>;
-    if (posts.length === 0) return (
-        <div className="border-muted-foreground/40 bg-muted/10 text-muted-foreground flex flex-col items-center justify-center gap-2 rounded-md border border-dashed py-5">
-            <span>Nenhuma noticia encontrada</span>
-        </div>
-    );
-
+    if (isLoading) return <div>Carregando notícias...</div>;
+    if (posts?.length === 0)
+        return (
+            <div className="border-muted-foreground/40 bg-muted/10 text-muted-foreground flex flex-col items-center justify-center gap-2 rounded-md border border-dashed py-5">
+                <span>Nenhuma noticia encontrada</span>
+            </div>
+        );
+    if (posts?.length && !selectedPost) setSelectedPost(posts[0]);
     return (
-        <div className="grid grid-cols-1 md:grid-cols-[60%_40%] gap-4">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-[60%_40%]">
             {/* |=======| DESTAQUE (GRANDE) |=======| */}
             <div className="h-full">
                 {selectedPost && <CardNoticias post={selectedPost} />}
             </div>
 
             {/* |=======| LISTA LATERAL |=======| */}
-            <div className="flex flex-col gap-3 justify-between">
-                {posts.slice(0, 3).map((post) => (
-                    <CardSecundariaNoticias 
-                        key={post.id} 
+            <div className="flex flex-col justify-between gap-3">
+                {posts?.slice(0, 3).map((post) => (
+                    <CardSecundariaNoticias
+                        key={post.id}
                         post={post}
                         isSelected={selectedPost?.id === post.id}
                         onSelect={() => setSelectedPost(post)}
