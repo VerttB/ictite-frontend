75d49f05ee4b61a8cfb6abb4fb325abd0cc5da50
Gabrielogui <gabrielcsro@gmail.com>
2026-03-02 10:08:03 -0300

    style: Incrementando e deixando login responsivo
    > Adicionando botão de voltar
    >> > Adicionando texto estilizado
    >> > Texto simulado no lugar de LoremPicsum
    >> > Tirando parte esquerda à telas menores

--- src/app/(paginas)/(auth)/layout.tsx
+++ src/app/(paginas)/(auth)/layout.tsx
@@ -8,20 +8,36 @@ export default function AuthLayout({
    
     return (
         <div className="bg-background flex h-screen">
-            <div className="bg-primary flex h-full w-full flex-col justify-between p-24 text-white">
-                <Image
-                    src={`${getAssetPrefix()}/nova_logo_ictite_04.png`}
-                    alt="Logo"
-                    width={300}
-                    height={100}
-                    className="object-contain"
-                />
-                <p className="font-font-secondary text-2xl">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
-                    numquam dolorum voluptate quaerat quas sit animi eum
-                    delectus necessitatibus earum impedit laudantium facilis
-                    possimus, suscipit labore amet quasi libero blanditiis.
-                </p>
+            <div className="hidden bg-primary lg:flex h-full w-full flex-col justify-between p-24 text-white">
+                
+                <div className="flex flex-col gap-2">
+                    <div>
+                        <Image
+                            src={`${getAssetPrefix()}/nova_logo_ictite_04.png`}
+                            alt="Logo"
+                            width={300}
+                            height={100}
+                            className="object-contain"
+                        />
+                    </div>
+                    <div className="flex flex-col text-4xl font-semibold leading-tight tracking-tight">
+                        <p>Impulsionando a</p>
+                        <p className="text-blue-300 font-black">Ciência nas Escolas</p>
+                        <p>da Bahia</p>
+                    </div>
+                    <div className="bg-secondary h-2 w-24 rounded-md"></div>
+                </div>
+                <div className="flex flex-col gap-1">
+                    <p className="font-medium italic text-2xl">
+                        A inovação e a educação científica são os motores fundamentais para o desenvolvimento regional sustentável e a transformação social.
+                    </p>
+                    <div className="flex gap-2 items-center">
+                        <div className="h-12 w-12 bg-secondary/20 rounded-full flex items-center justify-center">
+                            BA
+                        </div>
+                        <span className="font-medium text-xl">REDE ICTITE</span>
+                    </div>
+                </div>
             </div>
             <div className="h-full w-full">{children}</div>
         </div>
