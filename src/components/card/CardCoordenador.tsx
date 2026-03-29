3df006e17e732071b55cef2e99043f32a1e59ffb
Gabrielogui <gabrielcsro@gmail.com>
2026-03-29 11:42:39 -0300

    style: Colocando cor roxa no card + Adicionando ícones + melhorando UX da distribuição de area de atuação com clube

--- src/components/card/CardCoordenador.tsx
+++ src/components/card/CardCoordenador.tsx
@@ -1,4 +1,5 @@
 import { CoordinatorWithClub } from "@/core/domain/Coordinator"
+import { BrainCircuit, ScrollText } from "lucide-react";
 
 interface CoordinatorCardProps {
     coordinator: CoordinatorWithClub;
@@ -6,17 +7,25 @@ interface CoordinatorCardProps {
 
 export default function CardCoordenador({ coordinator }: CoordinatorCardProps) {
     return(
-        <div className="bg-background flex h-fit justify-between overflow-hidden rounded-md border border-l-8 border-l-lime-500">
+        <div className="bg-background flex h-fit justify-between overflow-hidden rounded-md border border-l-8 border-l-purple-800">
             <div className="flex flex-1 flex-col justify-center gap-4 p-5">
                 <h2 className="text-font-primary text-2xl font-semibold">
                     {coordinator.name}
                 </h2>
-                <p className="text-font-secondary max-h-28 overflow-y-auto text-justify text-sm">
-                    {coordinator.area}
-                </p>
-                <p className="text-font-secondary max-h-28 overflow-y-auto text-justify text-sm">
-                    {coordinator.clube.name}
-                </p>
+                <div className="flex flex-col gap-2">
+                    <div className="flex gap-2 items-center text-font-secondary">
+                        <ScrollText size={20}/>
+                        <p className=" max-h-28 overflow-y-auto text-justify text-sm">
+                            {coordinator.area}
+                        </p>
+                    </div>
+                    <div className="flex gap-2 items-center text-font-secondary">
+                        <BrainCircuit size={20}/>
+                        <p className=" max-h-28 overflow-y-auto text-justify text-sm">
+                            {coordinator.clube.name}
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
     )
