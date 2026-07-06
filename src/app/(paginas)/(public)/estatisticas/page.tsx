067f2362adb9ab905b305347a89f3445344ddedd
Gabrielogui <gabrielcsro@gmail.com>
2026-07-06 20:02:37 -0300

    feat: Colocando iframe do BI do mais ciência na página de estatísticas

---
+++ src/app/(paginas)/(public)/estatisticas/page.tsx
@@ -0,0 +1,11 @@
+export default function Estatisticas () {
+    return (
+        <div className="w-full h-full">
+            <iframe title="maisciencia" width="600" height="373.5" 
+                src="https://app.powerbi.com/view?r=eyJrIjoiYzJkZTdlZTItOGEzZi00ZWQzLThhYjYtYjQyNGNlYjdkZmE4IiwidCI6IjcyNjE3ZGQ4LTM3YTUtNDJhMi04YjIwLTU5ZDJkMGM1MDcwNyJ9" 
+                frameBorder="0" allowFullScreen="true"
+                className="w-full h-full">
+            </iframe>
+        </div>
+    );
+}
